85799692d54583cf4e2c017b3316165b4d526860
SaicereT <justintrejos50@yahoo.com>
2022-11-08 16:02:59 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,27 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  let combination = [];
+  let domains = "";
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let x = 0; x < noun.length; x++) {
+        //combination.push(pronoun[i] + adj[j] + noun[x] + ".com");
+        domains +=
+          '<li class="list-group-item">' +
+          pronoun[i] +
+          adj[j] +
+          noun[x] +
+          ".com" +
+          "</li>";
+      }
+    }
+  }
+
+  document.getElementById("domains").innerHTML = domains;
 };
